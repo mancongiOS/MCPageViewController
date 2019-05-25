d46be6eeeb0debf0c6332ec218dfe1e107218935
满聪 <cman@wisepool.com.cn>
2019-05-25 18:54:01 +0800

    监听scrollViewDidScroll

--- Example/Pods/Target Support Files/Pods-MCPageViewController_Example/Pods-MCPageViewController_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-MCPageViewController_Example/Pods-MCPageViewController_Example-frameworks.sh
@@ -153,10 +153,12 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/MCPageViewController/MCPageViewController.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/MJRefresh/MJRefresh.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SnapKit/SnapKit.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/MCPageViewController/MCPageViewController.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/MJRefresh/MJRefresh.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SnapKit/SnapKit.framework"
 fi
